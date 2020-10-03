d341e994a24c99aa9eb54aaf18cc3452322bb1a7
weizhongxin <wei.zhongxin@qq.com>
2020-10-03 09:55:51 +0800

    创建sp02-itemservice项目

---
+++ sp02-itemservice/src/main/java/cn/tedu/sp02/Application.java
@@ -0,0 +1,15 @@
+package cn.tedu.sp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Administrator
+ * @CreateTime 2020/10/3 - 09:49
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
