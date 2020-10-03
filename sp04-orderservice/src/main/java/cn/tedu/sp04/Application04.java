5b576bac06edb7a622697b2bc410664f9a4ff352
weizhongxin <wei.zhongxin@qq.com>
2020-10-03 10:44:34 +0800

    创建sp04-orderservice项目

---
+++ sp04-orderservice/src/main/java/cn/tedu/sp04/Application04.java
@@ -0,0 +1,15 @@
+package cn.tedu.sp04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Administrator
+ * @CreateTime 2020/10/3 - 10:40
+ */
+@SpringBootApplication
+public class Application04 {
+    public static void main(String[] args) {
+        SpringApplication.run(Application04.class, args);
+    }
+}
