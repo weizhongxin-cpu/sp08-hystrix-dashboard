11c4133ff6b19f1bdaab60b043119fe96e405610
weizhongxin <wei.zhongxin@qq.com>
2020-10-29 21:52:47 +0800

    ribbon重试—测试

--- sp06-ribbon/src/main/java/cn/tedu/sp06/Sp06RibbonApplication.java
+++ sp06-ribbon/src/main/java/cn/tedu/sp06/Sp06RibbonApplication.java
@@ -2,9 +2,12 @@ package cn.tedu.sp06;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Sp06RibbonApplication {
 
@@ -12,8 +15,12 @@ public class Sp06RibbonApplication {
 
 		SpringApplication.run(Sp06RibbonApplication.class, args);
 	}
+	@LoadBalanced
 	@Bean
 	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
+		SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+		factory.setConnectTimeout(1000);
+		factory.setReadTimeout(1000);
+		return new RestTemplate(factory);
 	}
 }
--- sp06-ribbon/src/main/java/cn/tedu/sp06/controller/RibbonController.java
+++ sp06-ribbon/src/main/java/cn/tedu/sp06/controller/RibbonController.java
@@ -1,54 +1,58 @@
 package cn.tedu.sp06.controller;
 
-import cn.tedu.sp01.pojo.Item;
-import cn.tedu.sp01.pojo.User;
-import cn.tedu.web.util.JsonResult;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
 import java.util.List;
 
-/**
- * Administrator
- * 2020/10/27 - 17:14
- */
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import cn.tedu.sp01.pojo.Item;
+import cn.tedu.sp01.pojo.Order;
+import cn.tedu.sp01.pojo.User;
+import cn.tedu.web.util.JsonResult;
+
+@Slf4j
 @RestController
-@RequestMapping("/")
 public class RibbonController {
-    private RestTemplate restTemplate;
     @Autowired
-    public void setRestTemplate(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-    @GetMapping("item-service/{orderId}")
-    public JsonResult<List<Item>> getItems(@PathVariable String orderId){
-        // 向指定微服务地址发送 get请求，并获得该服务的返回结果
-        // {1} 占位符，用 orderId填充
-        // 使用restTemplate工具，调用远程商品服务
-        return restTemplate.getForObject(
-                "http://localhost:8001/{1}",
-                JsonResult.class,
-                orderId);
+    private RestTemplate rt;
+
+    @GetMapping("/item-service/{orderId}")
+    public JsonResult<List<Item>> getItems(@PathVariable String orderId) {
+        //这里服务器路径用 service-id 代替，ribbon 会向服务的多台集群服务器分发请求
+        return rt.getForObject("http://item-service/{1}", JsonResult.class, orderId);
     }
 
-    @PostMapping("item-service/decreaseNumber")
-    public JsonResult<?> decreaseNumber(@RequestBody List<Item> items){
-        // 用restTemplate调用远程服务，发送post请求
-        // 商品数据放在协议体，向远程服务提交
-        return restTemplate.postForObject(
-                "http://localhost:8001/decreaseNumber",
-                items,
-                JsonResult.class
-        );
+    @PostMapping("/item-service/decreaseNumber")
+    public JsonResult decreaseNumber(@RequestBody List<Item> items) {
+        return rt.postForObject("http://item-service/decreaseNumber", items, JsonResult.class);
     }
 
-    @GetMapping("user-service/{userId}")
-    public JsonResult<User> getUser(@PathVariable Integer userId){
-        return restTemplate.getForObject(
-                "http://localhost:8001/{1}",
-                JsonResult.class,
-                userId
-        );
+
+    @GetMapping("/user-service/{userId}")
+    public JsonResult<User> getUser(@PathVariable Integer userId) {
+        return rt.getForObject("http://user-service/{1}", JsonResult.class, userId);
+    }
+
+    @GetMapping("/user-service/{userId}/score")
+    public JsonResult addScore(
+            @PathVariable Integer userId, Integer score) {
+        return rt.getForObject("http://user-service/{1}/score?score={2}", JsonResult.class, userId, score);
+    }
+
+
+    @GetMapping("/order-service/{orderId}")
+    public JsonResult<Order> getOrder(@PathVariable String orderId) {
+        return rt.getForObject("http://order-service/{1}", JsonResult.class, orderId);
+    }
+
+    @GetMapping("/order-service")
+    public JsonResult addOrder() {
+        return rt.getForObject("http://order-service/", JsonResult.class);
     }
 }
