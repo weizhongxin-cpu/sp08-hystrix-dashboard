2e2b60a4096c0e22db0a8d3a899838d24c7992cd
Administrator <weizhongxin666@163.com>
2020-11-21 20:09:05 +0800

    sp09-feign,添加FeignController

--- sp09-feigh/src/main/java/cn/tedu/sp09/controller/FeignController.java
+++ sp09-feigh/src/main/java/cn/tedu/sp09/controller/FeignController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -43,7 +44,7 @@ public class FeignController {
         return itemClient.getItems(orderId);
     }
     @GetMapping("/item-service/decreaseNumber")
-    public JsonResult<List<Item>> decreaseNumber(@PathVariable List<Item> items){
+    public JsonResult<List<Item>> decreaseNumber(@RequestBody List<Item> items){
         return itemClient.decreaseNumber(items);
     }
     @GetMapping("/user-service/{userId}")
