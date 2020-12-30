150ffea27d19f867277af192f1e4fff61e78626c
Administrator <weizhongxin666@163.com>
2020-12-30 21:37:27 +0800

    创建turbine项目

---
+++ sp09-turbine/src/main/java/cn/tedu/sp09turbine/Sp09TurbineApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp09turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp09TurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp09TurbineApplication.class, args);
+    }
+
+}
---
+++ sp09-turbine/src/test/java/cn/tedu/sp09turbine/Sp09TurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp09turbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp09TurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
